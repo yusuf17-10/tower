365f078d0b0e89229abdb15d7941fda83bdb7c7d
yusuf17-10 <64640684+yusuf17-10@users.noreply.github.com>
2020-09-01 12:20:28 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,17 @@
+class Box {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("red");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
---
+++ sketch.js
@@ -0,0 +1,72 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var ground;
+
+var slingShot,p1;
+
+var block1,block2,block3,block4,block5;
+var block6,block7,block8,block9,block10;
+var block11,block12,block13,block14,block15,block16;
+function preload(){
+  p1=loadImage("poly.png");
+}
+function setup() {
+  var canvas =  createCanvas(600,600);
+  engine = Engine.create();
+  world = engine.world;
+ ground = new Ground(400,500,400,10);
+
+ 
+ block8=new Box(330,235,30,40);
+ block9=new Box(360,235,30,40);
+ block10=new Box(390,235,30,40);
+ block11=new Box(420,235,30,40);
+ block12=new Box(450,235,30,40);
+ block13=new Box(360,195,30,40);
+ block14=new Box(390,195,30,40);
+ block15=new Box(420,195,30,40);
+ block16=new Box(390,155,30,40);
+ 
+ poly=Bodies.circle(50,200,20);
+ World.add(world,poly);
+
+ slingShot= new SlingShot(poly,{x:100,y:200});
+
+ 
+
+}
+
+
+
+function mouseDragged(){
+  Matter.Body.setPosition(poly,{x:mouseX,y:mouseY});
+}
+
+
+function mouseReleased(){
+   slingShot.fly();
+}
+
+function draw() {
+  background(255);  
+
+  Engine.update(engine);
+  image(p1,poly.position.x,poly.position.y,30,30);
+  block8.display();
+  block9.display();
+  block10.display();
+  block11.display();
+  block12.display();
+  block13.display();
+  block14.display();
+  block15.display();
+  block16.display();
+  slingShot.display();
+
+  ground.display();
+  
+}
+
